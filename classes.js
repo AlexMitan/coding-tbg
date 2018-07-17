5a58364d198241fcae1a69b0d1516b36ec1f4e82
alexmitan <alex.mitan@yahoo.com>
2018-07-17 19:06:34 +0300

    simulation with healing and decision making

--- classes.js
+++ classes.js
@@ -1,14 +1,14 @@
 "use strict";
 var utils = require('./cmutils.js');
-var { log, addToArrUnique, removeFromArr } = utils;
+var { log, addToArrUnique, removeFromArr, shuffle } = utils;
 
 
-function targetRandom(group) {
+function getRandom(group) {
     if (group.units.length > 0) {
         return utils.pickFrom(group.units);
     }
 }
-function targetMostWounded(group) {
+function getMostWounded(group) {
     if (group.units.length > 0) {
         let targets = group.units.slice().sort((unitA, unitB) => unitA.hp > unitB.hp);
         // log(`most wounded: ${targets}`);
@@ -16,7 +16,7 @@ function targetMostWounded(group) {
         // return group.units.slice().sort(unit => unit.hp)[0];
     }
 }
-function targetHealthiest(group) {
+function getHealthiest(group) {
     if (group.units.length > 0) {
         let targets = group.units.slice().sort((unitA, unitB) => unitA.hp < unitB.hp);
         // log(`healthiest: ${targets}`);
@@ -37,28 +37,42 @@ class Ability {
     constructor(name, unit) {
         this.name = name;
         this.unit = unit;
+        this.cost = 0;
+    }
+    enoughAp() {
+        return this.unit.ap >= this.cost;
     }
     use(target) {
     }
 }
 
 class Attack extends Ability {
-    constructor(unit, dmg) {
+    constructor(unit, dmg, cost) {
         super('attack', unit); 
         this.dmg = dmg;
+        this.cost = cost;
     }
     use(target) {
-        target && target.receiveDamage(this.dmg);
+        // expend ap if proper target
+        if (target !== undefined && this.enoughAp()) {
+            this.unit.ap -= this.cost;
+            target.receiveDamage(this.dmg);
+        }
     }
 }
 
 class Heal extends Ability {
-    constructor(unit, amount) {
+    constructor(unit, amount, cost) {
         super('heal', unit);
         this.amount = amount;
+        this.cost = cost;
     }
     use(target) {
-        target.hp = Math.min(target.basehp, target.hp + this.amount);
+        // expend ap if proper target
+        if (target !== undefined && this.enoughAp()) {
+            this.unit.ap -= this.cost;
+            target.hp = Math.min(target.baseHp, target.hp + this.amount);
+        }
     }
 }
 
@@ -70,10 +84,11 @@ class Wait extends Ability {
 
 class Unit {
     constructor(hp, ap) {
-        this.basehp = hp;
+        this.baseHp = hp;
         this.hp = hp;
-        this.baseap = ap;
+        this.baseAp = ap;
         this.ap = ap;
+        this.apRegen = 1;
         this.abilities = {};
     }
     // set group(val) {}
@@ -83,6 +98,9 @@ class Unit {
     use(abilityName, ...args) {
         this.abilities[abilityName].use(...args);
     }
+    update() {
+        this.ap = Math.min(this.ap + this.apRegen, this.baseAp);
+    }
     receiveDamage(dmg) {
         this.hp -= dmg;
         if (this.hp <= 0) {
@@ -91,15 +109,15 @@ class Unit {
         }
     }
     toString() {
-        return `[${this.hp}]`;
-        // return `${this.hp < this.basehp ? 'u' : 'U'}`;
+        return `[${this.hp} ${'.'.repeat(this.ap)}]`;
+        // return `${this.hp < this.baseHp ? 'u' : 'U'}`;
     }
 }
 
 function makeDrone(hp, ap, dmg) {
     let unit = new Unit(hp, ap);
-    unit.addAbility(new Attack(unit, dmg));
-    unit.addAbility(new Heal(unit, 3));
+    unit.addAbility(new Attack(unit, dmg, 3));
+    unit.addAbility(new Heal(unit, 1, 2));
     return unit;
 }
 
@@ -128,7 +146,7 @@ let allies = new Group();
 let enemies = new Group();
 // let stat = (i) => utils.randomInt(1, i);
 let hpI = (i) => 9;
-let apI = (i) => 4;
+let apI = (i) => 10;
 let dmgI = (i) => 1;
 for (let i=1; i<20; i++) {
     // if (i > 5)
@@ -137,7 +155,7 @@ for (let i=1; i<20; i++) {
 }
 // let ship = new Unit(3, 3);
 // log(ship);
-// ship.attack(targetHealthiest(allies));
+// ship.attack(getHealthiest(allies));
     
 let turns = 0;
 while (allies.units.length > 0 && enemies.units.length > 0) {
@@ -149,21 +167,23 @@ while (allies.units.length > 0 && enemies.units.length > 0) {
     // 0 turn
     if (allies.units.length > 0) {
         for (let unit of allies.units) {
-            // unit.use('attack', targetRandom(enemies));
-            unit.use('attack', targetHealthiest(enemies));
-            // unit.use('attack', targetMostWounded(enemies));
+            unit.update();
+            // unit.use('attack', getRandom(enemies));
+            // unit.use('attack', getHealthiest(enemies));
+            unit.use('attack', getMostWounded(enemies));
         }
     }
     // 1 turn
     if (enemies.units.length > 0) {
         for (let unit of enemies.units) {
-            // unit.use('attack', targetRandom(allies));
-            if (Math.random() < 0.5) {
-                unit.use('attack', targetHealthiest(allies));
+            unit.update();
+            unit.use('attack', getRandom(allies));
+            if (getMostWounded(enemies).hp < 5) {
+                unit.use('heal', getRandom(enemies));
             } else {
-                unit.use('heal', targetHealthiest(enemies));
+                unit.use('attack', getHealthiest(allies));
             }
-            // unit.use('attack', targetMostWounded(allies));
+            // unit.use('attack', getMostWounded(allies));
         }
     }
 }
